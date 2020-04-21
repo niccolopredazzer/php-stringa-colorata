e60597e70a36450a21a9acccc7fecda8e8308ead
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-04-21 20:43:32 +0200

    Esercizio finito

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $password = $_GET['password'];
+    if ($password == 'Boolean') {
+        echo "<p style='color:green'>verde</p>";
+    } else {
+        echo "<p style='color:red'>rosso</p>";
+    }
+
+ ?>
